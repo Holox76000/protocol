19887f311f421bea6cd175b98c8c2f60f2de0717
Holox76000 <70341785+Holox76000@users.noreply.github.com>
2026-03-07 21:03:40 +0400

    Install Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,13 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 fbq('init', '1889105658401390');
 fbq('track', 'PageView');`}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "vs4urd7zuk");`}
+        </Script>
         <noscript>
           <img
             height="1"
